940b3261b00069f662f5d4ecb4f10f35f57a8aa1
reham15 <rehamnader0123@gmail.com>
2022-04-29 00:27:06 +0200

    nodejs lab5

---
+++ ITI/nodejs/lab5/app.js
@@ -0,0 +1,26 @@
+
+
+var express = require('express')
+const path = require("path");
+var app = new express()
+var http = require('http').createServer(app)
+var io = require('socket.io')(http)
+app.use(express.static(path.join(__dirname, 'static')));
+app.get('/', function(req, res){
+    res.sendFile(__dirname+'/index.html')
+})
+app.use(express.static(path.join(__dirname, 'static')));
+io.on('connect', function(client){
+    client.emit('connected');
+    client.on('join', function(name){
+    	client.nickname = name;
+      console.log(client.nickname)
+    })
+client.on('message', function(data){
+ var nickname = client.nickname ;
+    client.broadcast.emit('messages', data) //--> all clients except
+})
+})
+
+
+http.listen(3000);
