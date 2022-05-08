81ecdf7a7d50e9ed1de3caeaef75f0ae86f35e3f
reham15 <rehamnader0123@gmail.com>
2022-05-08 15:01:45 +0200

    lab1 angylar

---
+++ ITI/angular/lab1/src/app/student/student.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'student',
+  template:`
+  <p> student component</p>`,
+
+})
+export class StudentComponent {
+  title = 'lab1';
+}
